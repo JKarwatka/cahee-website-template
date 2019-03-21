be488593ed8d803f47a55894fa68646f2fc8d1ef
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-03-21 23:16:07 +0100

    add animations, fix layout problems, remove JS smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("li>a").forEach(element => {
-    element.addEventListener("click", e => {
-      e.preventDefault();
-
-      document.querySelector(element.hash).scrollIntoView({
-        behavior: "smooth"
-      });
-    });
-  });
 
   const navbar = document.querySelector(".header__navbar");
 
   window.addEventListener("scroll", e => {
-    e.preventDefault();
     if (window.pageYOffset > 100) {
-      navbar.classList += " header__navbar--shrink";
+      if (!navbar.classList.contains(" header__navbar--shrink")){
+        navbar.classList += " header__navbar--shrink";
+      }
     } else {
       navbar.classList = "header__navbar";
     }
-  });
+  }, {
+      capture: true,
+      passive: true
+    });
 });
 
 console.log(document.querySelectorAll("a"));
