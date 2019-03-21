c7a58410e88be6fa4dde223afaf8bab1d94f8be8
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-03-21 16:50:03 +0100

    fix smooth scroll, add shrirnking navbar on scroll,

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector('a').forEach(element => {
-    element.scrollIntoView({behavior: 'smooth'});
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("li>a").forEach(element => {
+    element.addEventListener("click", e => {
+      e.preventDefault();
+
+      document.querySelector(element.hash).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+
+  const navbar = document.querySelector(".header__navbar");
+
+  window.addEventListener("scroll", e => {
+    e.preventDefault();
+    if (window.pageYOffset > 100) {
+      navbar.classList += " header__navbar--shrink";
+    } else {
+      navbar.classList = "header__navbar";
+    }
+  });
 });
-});
+
+console.log(document.querySelectorAll("a"));
